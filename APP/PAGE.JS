b7f5116e88cfcc0af8c64db44eab0109c55538ec
jaretlewis007-blip <jaretlewis007@gmail.com>
2025-10-27 20:24:47 -0500

    Create Home component in PAGE.JS

---
+++ APP/PAGE.JS
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 32 }}>
+      <h1 style={{ fontSize: 32, marginBottom: 12 }}>IdeaVault</h1>
+      <p>Deployment OK. Next step: wire Firebase + NDA.</p>
+    </main>
+  );
+}
