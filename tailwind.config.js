9e9d770da22c9ad7539fe8e59c4ec3160b556f34
jaretlewis007-blip <jaretlewis007@gmail.com>
2025-10-27 19:40:13 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#D4AF37',
+        silver: '#C0C0C0',
+        black: '#0A0A0A'
+      },
+    },
+  },
+  plugins: [],
+}
