4e77d32755fd2326c54b72d6cf534c3a49070049
jaretlewis007-blip <jaretlewis007@gmail.com>
2025-10-28 08:53:17 -0500

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: AIzaSyDdckYXQ67QxrfSUVGt9L0GI0HLu4gc-YQ
-  authDomain: ideavault-web.firebaseapp.com
-  projectId: ideavault-web
-  storageBucket: ideavault-web.firebasestorage.app
-  messagingSenderId: 1045044544401
-  appId: 1:1045044544401:web:46ee58643ab433a1101030
+  apiKey: "AIzaSyDdckYXQ67QxrfSUVGt9L0GI0HLu4gc-YQ",
+  authDomain: "ideavault-web.firebaseapp.com",
+  projectId: "ideavault-web",
+  storageBucket: "ideavault-web.firebasestorage.app",
+  messagingSenderId: "1045044544401",
+  appId: "1:1045044544401:web:46ee58643ab433a1101030",
 };
 
 const app = initializeApp(firebaseConfig);
